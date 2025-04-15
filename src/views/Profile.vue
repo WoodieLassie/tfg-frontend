81948157db6ddd656e408751dfbe5e69abef1dfd
Judith Coello Quesada <jcoelloquesada@gmail.com>
2025-04-15 11:25:20 +0200

    Terminado de implementar perfil de usuario

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,8 +1,21 @@
 <template>
-    <section v-if="user" class="user__info">
-        <h1>Perfil</h1> 
-        <h4>Username:</h4>
-        <p>{{ user.email }}</p>
+    <section v-if="user" class="user__profile">
+        <article class="user__info">
+            <h1>Perfil</h1> 
+            <h4>Username:</h4>
+            <p>{{ user.email }}</p>
+        </article>
+    </section>
+    <section v-if="userFavourites" class="user__favourites">
+        <article class="user__favourite" v-for = "userFavourite in userFavourites">
+            <h2>Lista de favoritos</h2>
+            <router-link :to="{name: 'shows', params: {id: userFavourite.show.id}}">
+            <div>
+                <h2>{{ userFavourite.show.name }}</h2>
+                <p>{{ userFavourite.show.description }}</p>
+            </div>
+            </router-link>
+        </article>
     </section>
 </template>
 
@@ -16,8 +29,11 @@
                 userFavourites: null
             }
         },
+        props: {
+            id: {type: String, required: true}
+        },
         async created() {
-            const data = await fetch(`http://localhost:8080/api/users`, {
+            const data = await fetch(`http://localhost:8080/api/users/${this.$props.id}`, {
             method: 'get',
             headers: {
                 'Accept': 'application/json',
@@ -27,6 +43,16 @@
             const response = await data.json()
             this.user = response
         },
+        async mounted() {
+            const data = await fetch (`http://localhost:8080/api/favourites/${this.$props.id}`, {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }})
+            const response = await data.json()
+            this.userFavourites = response
+        }
         // async mounted() {
         //     const data = await fetch("http://localhost:8080/api/comments")
         // }
